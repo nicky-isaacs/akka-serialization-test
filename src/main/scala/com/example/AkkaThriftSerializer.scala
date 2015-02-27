9f14c8b1b32da327c4f47a3245ba7f026fb36b1f
Nick Isaacs <nick@victorops.com>
2015-02-27 11:39:29 -0700

    support all thirft messages

--- src/main/scala/com/example/AkkaThriftSerializer.scala
+++ src/main/scala/com/example/AkkaThriftSerializer.scala
@@ -2,23 +2,30 @@ package com.example
 
 import com.twitter.bijection.scrooge.BinaryScalaCodec
 import akka.serialization.Serializer
-import victorops.thrift.scala.{Color, NastyCaseClass}
+import com.twitter.scrooge.{ThriftStructCodec, ThriftStruct}
+import victorops.thrift.scala.{FeetSize, Color, NastyCaseClass}
 
 
 class AkkaThriftSerializer extends Serializer {
-
   def includeManifest: Boolean = true
   def identifier = 34543
 
   def toBinary(obj: AnyRef): Array[Byte] = obj match {
-    case m: NastyCaseClass => BinaryScalaCodec(NastyCaseClass)(m)
-    case m: Color => BinaryScalaCodec(Color)(m)
+    case n: NastyCaseClass => BinaryScalaCodec(NastyCaseClass)(n)
+    case c: Color          => BinaryScalaCodec(Color)(c)
+    case f: FeetSize       => BinaryScalaCodec(FeetSize)(f)
     case m => throw new IllegalArgumentException("Cannot serialize object: " + m)
   }
 
   def fromBinary(bytes: Array[Byte], clazz: Option[Class[_]]): AnyRef = clazz match {
     case Some(c) if c == classOf[NastyCaseClass.Immutable] => BinaryScalaCodec(NastyCaseClass).invert(bytes).get
-    case Some(c) => throw new IllegalArgumentException("Cannot deserialize class: " + c.getCanonicalName)
-    case None => throw new IllegalArgumentException("No class found in EventSerializer when deserializing array: " + bytes.mkString(""))
+    case Some(c) if c == classOf[Color.Immutable]          => BinaryScalaCodec(Color).invert(bytes).get
+    case Some(c) if c == classOf[FeetSize.Immutable]       => BinaryScalaCodec(FeetSize).invert(bytes).get
+
+    case Some(c)                                           =>
+      throw new IllegalArgumentException("Cannot deserialize class: " + c.getCanonicalName)
+
+    case None                                              =>
+      throw new IllegalArgumentException("No class found in EventSerializer when deserializing array: " + bytes.mkString(""))
   }
 }
