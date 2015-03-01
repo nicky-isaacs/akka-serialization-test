82398b4832a0b7e02cedc784733f84c5e6391f76
Nick Isaacs <nick@victorops.com>
2015-03-01 09:01:57 -0700

    Log when we do a custom serialization

--- src/main/scala/com/example/AkkaThriftSerializer.scala
+++ src/main/scala/com/example/AkkaThriftSerializer.scala
@@ -10,22 +10,28 @@ class AkkaThriftSerializer extends Serializer {
   def includeManifest: Boolean = true
   def identifier = 34543
 
-  def toBinary(obj: AnyRef): Array[Byte] = obj match {
-    case n: NastyCaseClass => BinaryScalaCodec(NastyCaseClass)(n)
-    case c: Color          => BinaryScalaCodec(Color)(c)
-    case f: FeetSize       => BinaryScalaCodec(FeetSize)(f)
-    case m => throw new IllegalArgumentException("Cannot serialize object: " + m)
+  def toBinary(obj: AnyRef): Array[Byte] = {
+    println("Doing a custom serialization")
+    obj match {
+      case n: NastyCaseClass => BinaryScalaCodec(NastyCaseClass)(n)
+      case c: Color          => BinaryScalaCodec(Color)(c)
+      case f: FeetSize       => BinaryScalaCodec(FeetSize)(f)
+      case m => throw new IllegalArgumentException("Cannot serialize object: " + m)
+    }
   }
 
-  def fromBinary(bytes: Array[Byte], clazz: Option[Class[_]]): AnyRef = clazz match {
-    case Some(c) if c == classOf[NastyCaseClass.Immutable] => BinaryScalaCodec(NastyCaseClass).invert(bytes).get
-    case Some(c) if c == classOf[Color.Immutable]          => BinaryScalaCodec(Color).invert(bytes).get
-    case Some(c) if c == classOf[FeetSize.Immutable]       => BinaryScalaCodec(FeetSize).invert(bytes).get
+  def fromBinary(bytes: Array[Byte], clazz: Option[Class[_]]): AnyRef = {
+    println("Doing a custom de-serialization")
+    clazz match {
+      case Some(c) if c == classOf[NastyCaseClass.Immutable] => BinaryScalaCodec(NastyCaseClass).invert(bytes).get
+      case Some(c) if c == classOf[Color.Immutable]          => BinaryScalaCodec(Color).invert(bytes).get
+      case Some(c) if c == classOf[FeetSize.Immutable]       => BinaryScalaCodec(FeetSize).invert(bytes).get
 
-    case Some(c)                                           =>
-      throw new IllegalArgumentException("Cannot deserialize class: " + c.getCanonicalName)
+      case Some(c)                                           =>
+        throw new IllegalArgumentException("Cannot deserialize class: " + c.getCanonicalName)
 
-    case None                                              =>
-      throw new IllegalArgumentException("No class found in EventSerializer when deserializing array: " + bytes.mkString(""))
+      case None                                              =>
+        throw new IllegalArgumentException("No class found in EventSerializer when deserializing array: " + bytes.mkString(""))
+    }
   }
 }
